95762f70317aaff757765bfbd92c51e700dbac8e
ViacheslavYUR <108462275+ViacheslavYUR@users.noreply.github.com>
2022-12-05 21:03:27 +0200

    fix task3 after mentor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
@@ -7,15 +7,16 @@ const formRef = {
   message: document.querySelector('.feedback-form').message,
   form: document.querySelector('.feedback-form'),
 };
-const formData = {};
+let formData;
 
-formRef.email.addEventListener('input', throttle(onInput, 500));
-formRef.message.addEventListener('input', throttle(onInput, 500));
+formRef.form.addEventListener('input', throttle(onInput, 500));
 formRef.form.addEventListener('submit', onSubmitForm);
 
 function onInput(e) {
-  formData[e.target.name] = e.target.value;
-
+  formData = {
+    email: formRef.email.value,
+    message: formRef.message.value,
+  };
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
@@ -29,13 +30,76 @@ function onSubmitForm(evt) {
 function populateForm() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
   if (savedMessage) {
-    console.log(savedMessage);
+    // console.log(savedMessage);
     console.log(JSON.parse(savedMessage));
 
     const { email, message } = JSON.parse(savedMessage);
-    formRef.email.value = email || '';
+    formRef.email.value = email;
     formRef.message.textContent = message;
   }
 }
 populateForm();
 // console.log(formRef);
+// ==========================================================================================================
+
+// import throttle from 'lodash.throttle';
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   email: document.querySelector('.feedback-form').email,
+//   message: document.querySelector('.feedback-form').message,
+// };
+
+// // Записываем значения в локальное хранилище =====================================================
+
+// let FeedbackData;
+
+// const saveFeedbackDataToLocalStorage = e => {
+//   FeedbackData = {
+//     email: refs.email.value,
+//     message: refs.message.value,
+//   };
+
+//   localStorage.setItem('feedback-form-state', JSON.stringify(FeedbackData));
+// };
+
+// refs.form.addEventListener(
+//   'input',
+//   throttle(saveFeedbackDataToLocalStorage, 500)
+// );
+
+// // Возвращаем значения в форму =====================================================
+
+// if (localStorage.getItem('feedback-form-state')) {
+//   refs.email.value = JSON.parse(
+//     localStorage.getItem('feedback-form-state')
+//   ).email;
+//   refs.message.value = JSON.parse(
+//     localStorage.getItem('feedback-form-state')
+//   ).message;
+// }
+
+// // Обрабатываем событие Submit =====================================================
+
+// const onSubmitFormEvent = e => {
+//   event.preventDefault();
+
+//   if (localStorage.getItem('feedback-form-state')) {
+//     localStorage.removeItem('feedback-form-state');
+//   }
+
+//   if (refs.email.value && refs.message.value) {
+//     FeedbackData = {
+//       email: refs.email.value,
+//       message: refs.message.value,
+//     };
+//   } else {
+//     window.alert('The form is not completed! You need to fill it!');
+//   }
+
+//   console.log(FeedbackData);
+
+//   refs.form.reset();
+// };
+
+// refs.form.addEventListener('submit', onSubmitFormEvent);
